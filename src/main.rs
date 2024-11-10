c5b4a52147426d3dfe60d11de0a2a6a081d9a9e6
Giovanni Rella <rellag@nida.nih.gov>
2024-11-09 21:24:00 -0500

    testing adding a router

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use leptos::*;
+use leptos_meta::*;
+use leptos_router::*;
 
 #[component]
 fn App(increment: i32) -> impl IntoView {
@@ -32,7 +34,76 @@ fn App(increment: i32) -> impl IntoView {
 fn main() {
     mount_to_body(|| {
         view! {
-            <App increment=5 />
+
+        <Html lang="en" dir="ltr" attr:data-theme="light"/>
+
+        // sets the document title
+        <Title text="gio"/>
+
+        // injects metadata in the <head> of the page
+        <Meta charset="UTF-8"/>
+        <Meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+        <Router>
+            <Routes>
+                <Route path="/" view=Home/>
+                <Route path="/*" view=NotFound/>
+            </Routes>
+        </Router>
         }
     })
+}
+
+/// 404 Not Found Page
+#[component]
+pub fn NotFound() -> impl IntoView {
+    view! { <h1>"Uh oh!" <br/> "We couldn't find that page!"</h1>  }
+}
+
+
+/// Default Home Page
+#[component]
+pub fn Home() -> impl IntoView {
+    view! {
+        <ErrorBoundary fallback=|errors| {
+            view! {
+                <h1>"Uh oh! Something went wrong!"</h1>
+
+                <p>"Errors: "</p>
+                // Render a list of errors as strings - good for development purposes
+                <ul>
+                    {move || {
+                        errors
+                            .get()
+                            .into_iter()
+                            .map(|(_, e)| view! { <li>{e.to_string()}</li> })
+                            .collect_view()
+                    }}
+
+                </ul>
+            }
+        }>
+
+            <div class="container">
+
+                <picture>
+                    <source
+                        srcset="https://raw.githubusercontent.com/leptos-rs/leptos/main/docs/logos/Leptos_logo_pref_dark_RGB.svg"
+                        media="(prefers-color-scheme: dark)"
+                    />
+                    <img
+                        src="https://raw.githubusercontent.com/leptos-rs/leptos/main/docs/logos/Leptos_logo_RGB.svg"
+                        alt="Leptos Logo"
+                        height="200"
+                        width="400"
+                    />
+                </picture>
+
+                <h1>"Welcome to my site!"</h1>
+                <p>"A site written in Rust using "<a href="https://github.com/leptos-rs/leptos" target="_blank">"Leptos"</a>" deployed on Github Pages"</p>
+                <p>"The backend will also be written in Rust and deployed on AWS Lambda"</p>
+            </div>
+            <App increment=5 />
+        </ErrorBoundary>
+    }
 }
